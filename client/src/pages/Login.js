0f7af421f6289fc4ad003d4f0ad246ac0ac75698
Arief Daffa <ariefkeren76@yahoo.com>
2021-12-17 18:25:20 +0700

    add dummy data badut

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,10 +4,41 @@ import logo from '../assets/logo/Logo.png';
 import Card from '../layout/Card';
 import { useForm } from 'react-hook-form';
 import Button from '../layout/Button';
+import { auth } from '../data/staticData';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 	const { register, handleSubmit } = useForm();
-	const onSubmit = (data) => console.log(data);
+	const navigate = useNavigate();
+	const onSubmit = (data) => {
+		if (auth.nim === data.nim) {
+			if (auth.password === data.password) {
+				Swal.fire({
+					icon: 'success',
+					title: 'Login Berhasil',
+					timer: 2000,
+					timerProgressBar: true,
+				}).then((res) => {
+					navigate('/');
+				});
+			} else {
+				Swal.fire({
+					icon: 'error',
+					title: 'Password Salah',
+					timer: 2000,
+					timerProgressBar: true,
+				});
+			}
+		} else {
+			Swal.fire({
+				icon: 'error',
+				title: 'Nim tidak terdaftar',
+				timer: 2000,
+				timerProgressBar: true,
+			});
+		}
+	};
 	return (
 		<div className="bg-login-bg bg-no-repeat bg-center bg-cover">
 			<Container center={true}>
@@ -19,12 +50,12 @@ const Login = () => {
 						<form onSubmit={handleSubmit(onSubmit)} className="pb-20 pt-10">
 							<div className="text-center pb-10 font-bold text-5xl">Login</div>
 							<div className="px-2 md:px-20">
-								<label>Email</label>
+								<label>Nim</label>
 								<input
 									className="border-2 rounded-md w-full p-2 my-2"
-									placeholder="Masukkan Email"
-									type="email"
-									{...register('email')}
+									placeholder="Masukkan Nim"
+									type="text"
+									{...register('nim')}
 								/>
 							</div>
 							<div className="px-2 md:px-20">
--- client/src/pages/jadwalPages/JadwalPages.js
+++ client/src/pages/jadwalPages/JadwalPages.js
@@ -1,12 +1,86 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import Container from '../../layout/Container';
+import { jadwal1 } from '../../data/staticData';
 
 const JadwalPages = () => {
 	return (
 		<>
 			<Navbar jadwal />
-			<Container></Container>
+			<Container>
+				<div className="font-semibold p-5" style={{ fontSize: '45px' }}>
+					Jadwal Kamu
+				</div>
+				<div className="p-5 w-full">
+					<div
+						style={{
+							border: '1px solid',
+							boxShadow: '0px 2px 11px rgba(0, 0, 0, 0.25)',
+						}}
+					>
+						<div className="p-5">
+							<div
+								style={{
+									fontWeight: '500',
+									fontSize: '36px',
+									lineHeight: '24px',
+								}}
+							>
+								GG Gaming
+							</div>
+							<div className="flex flex-row justify-between py-5">
+								<div className=" w-full mr-10">
+									<table className="w-full border border-black">
+										<tr className="border border-black">
+											<th className="p-5">Hari</th>
+											<th className="py-2 border border-black">Jam</th>
+											<th className="py-2 border border-black">Kelas</th>
+											<th className="py-2 border border-black">Kode</th>
+											<th className="py-2 border border-black">Mata Kuliah</th>
+											<th className="py-2 border border-black">SKS</th>
+										</tr>
+										{jadwal1.map((data, id) => (
+											<tr
+												className="text-center border border-black"
+												key={id}
+												style={{ backgroundColor: data.kodeWarna }}
+											>
+												<td className="py-2 border border-black">
+													{data.hari}
+												</td>
+												<td className="py-2 border border-black">{data.jam}</td>
+												<td className="py-2 border border-black">
+													{data.kelas}
+												</td>
+												<td className="py-2 border border-black">
+													{data.kode}
+												</td>
+												<td className="py-2 border border-black">
+													{data.namaMataKuliah}
+												</td>
+												<td className="py-2 border border-black">{data.SKS}</td>
+											</tr>
+										))}
+									</table>
+								</div>
+								<div className="w-96">
+									<div>
+										{jadwal1.map((data, id) => (
+											<div className="flex py-2">
+												<div
+													className="w-5 h-5 rounded-full"
+													style={{ backgroundColor: data.kodeWarna }}
+												></div>
+												<div>{data.namaMataKuliah}</div>
+											</div>
+										))}
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</Container>
 		</>
 	);
 };
